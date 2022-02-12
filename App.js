36a9bb7b46d4ecac9a0e5a0c67866f5c09de3c8a
Harry <payalsinghal67@gmail.com>
2022-02-12 19:21:32 +0530

    Project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Project extends React.Component{
+    render(){
+        return(
+            <div id="root">
+                <p>
+                "I am learning React. My life is getting better. "
+            </p>
+            </div>
+        );
+            }
+}
+export default Project;
